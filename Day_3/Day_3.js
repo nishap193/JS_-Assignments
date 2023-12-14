a30ff1bded006fd176fd206e56ed0f7c30be74b5
nishap193 <138564419+nishap193@users.noreply.github.com>
2023-12-14 14:28:16 +0530

    Add files via upload

---
+++ Day_3/Day_3.js
@@ -0,0 +1,28 @@
+// string
+var str1 = 'Hello, World!'; // Output: Hello, World! 
+console.log(str1);
+
+// number
+var a=47;  // positive integer value
+var b=629.5;  // floating point
+var c=-5; // number with negative value
+
+// boolean
+var a1 =10;
+var b1=9;
+a1==b1 // returns false
+
+// undefined
+var x;   // undefined
+console.log(x); // returns undefined
+
+// Null
+var x = null;
+console.log(x);   // returns null
+
+// Symbol
+const sym = Symbol('uniqueSymbol');
+
+// object
+var student = {firstName: 'john', class: 7};
+console.log(student);
